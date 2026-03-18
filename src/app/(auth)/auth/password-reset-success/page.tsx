fa40d0631dec52149d15f2530e0b0c2da68d132c
murad403 <mdm78774@gmail.com>
2026-03-18 10:15:11 +0600

    refactor PasswordResetSuccess component layout; enhance icon styling and structure

--- src/app/(auth)/auth/password-reset-success/page.tsx
+++ src/app/(auth)/auth/password-reset-success/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { MdCheck } from "react-icons/md";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -10,10 +10,10 @@ const PasswordResetSuccess = () => {
   return (
     <div className="w-full flex items-center justify-center">
       <div className="w-full max-w-117.5 rounded-xl border border-border-color bg-[#FAFAFA] px-6 py-10 md:p-5">
-        <div>
-            <MdCheck  />
+        <div className="bg-[#17A31A] inline-block p-2 rounded-full">
+            <MdCheck className="size-10 text-main"/>
         </div>
-        <div className="text-center">
+        <div className="">
           <h1 className="text-2xl md:text-3xl leading-11 font-semibold text-title">
             Your Password Successfully Changed
           </h1>
